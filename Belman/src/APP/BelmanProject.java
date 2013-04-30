b452dedcd71d7de59407ae561e239ecb96df7d98
Feylin <Feylin@192.168.223.115>
2013-04-30 10:58:41 +0200

    Belman Updates

--- Belman/src/APP/BelmanProject.java
+++ Belman/src/APP/BelmanProject.java
@@ -2,9 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package belman.project;
+package APP;
 
-import GUI.BelmanStartside;
+import GUI.Overview;
 import java.awt.EventQueue;
 import javax.swing.UIManager;
 
