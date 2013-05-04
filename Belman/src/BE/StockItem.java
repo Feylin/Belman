f95af2ddb0ec0ae541fd772db282c3f0d503f9d5
Makja <mak.j@hotmail.com>
2013-05-04 20:55:56 +0200

    Færdig med stock list og information

--- Belman/src/BE/StockItem.java
+++ Belman/src/BE/StockItem.java
@@ -17,8 +17,10 @@ public class StockItem
     private double stockQuantity;
     private int coilTypeId;
     private int sleeveId;
+    private CoilType coilType;
+    private Material material;
 
-    public StockItem(int id, String chargeNo, double length, double stockQuantity, int coilTypeId, int sleeveId)
+    public StockItem(int id, String chargeNo, double length, double stockQuantity, int coilTypeId, int sleeveId, CoilType coilType, Material material)
     {
         this.id = id;
         this.chargeNo = chargeNo;
@@ -26,6 +28,8 @@ public class StockItem
         this.stockQuantity = stockQuantity;
         this.coilTypeId = coilTypeId;
         this.sleeveId = sleeveId;
+        this.coilType = coilType;
+        this.material = material;
 
     }
 
@@ -36,7 +40,9 @@ public class StockItem
                 item.getLength(),
                 item.getStockQuantity(),
                 item.getCoilTypeId(),
-                item.getSleeveId());
+                item.getSleeveId(),
+                item.getCoilType(),
+                item.getMaterial());                
     }
 
     /**
@@ -125,5 +131,37 @@ public class StockItem
     public void setSleeveId(int sleeveId)
     {
         this.sleeveId = sleeveId;
+    }
+
+    /**
+     * @return the coilType
+     */
+    public CoilType getCoilType()
+    {
+        return coilType;
+    }
+
+    /**
+     * @param coilType the coilType to set
+     */
+    public void setCoilType(CoilType coilType)
+    {
+        this.coilType = coilType;
+    }
+
+    /**
+     * @return the material
+     */
+    public Material getMaterial()
+    {
+        return material;
+    }
+
+    /**
+     * @param material the material to set
+     */
+    public void setMaterial(Material material)
+    {
+        this.material = material;
     }
 }
