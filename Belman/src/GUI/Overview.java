e30ec7ea7490b1d6627a0de3eabf48ecb424b29f
Feylin <Feylin@192.168.223.115>
2013-05-13 12:26:41 +0200

    Belman Refactoring (Row -1)

--- Belman/src/GUI/Overview.java
+++ Belman/src/GUI/Overview.java
@@ -314,7 +314,13 @@ public class Overview extends javax.swing.JFrame implements Observer
                 public void valueChanged(ListSelectionEvent es)
                 {
                     int selectedRow = tblSleeveList.getSelectedRow();
+                    if (selectedRow == -1)
+                    {
+                        return;
+                    }
+                    
                     Sleeve s = slmodel.getEventsByRow(selectedRow);
+                    
                     try
                     {
                         if (!omgr.getOrdersBySleeve(s).isEmpty())
@@ -1439,12 +1445,13 @@ public class Overview extends javax.swing.JFrame implements Observer
         OperatorSettings.getInstance().setVisible(true);
     }//GEN-LAST:event_itemSettingsActionPerformed
 
+    // RESET BUTTON DOES NOT WORK!!!!!!
     private void btnResetActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_btnResetActionPerformed
     {//GEN-HEADEREND:event_btnResetActionPerformed
        
         try
         {   
-            tblSleeveList.clearSelection();               
+                         
             smodel = new StockListTableModel(smgr.getAll()); 
             tblStockItem.setModel(smodel);
             omodel2 = new OrderTablemodel(omgr.getAll());
@@ -1455,7 +1462,11 @@ public class Overview extends javax.swing.JFrame implements Observer
         catch (Exception ex)
         {
            ex.getMessage();
-        }            
+        }
+        finally
+        {
+            tblSleeveList.clearSelection();  
+        }
         
     }//GEN-LAST:event_btnResetActionPerformed
 
