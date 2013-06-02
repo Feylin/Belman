4027a3b2ab9db94a7580df49d28e7e9be75dbda3
Administrator <Administrator@Faylin>
2013-06-02 22:27:06 +0200

    Belman - Refactoring

--- Belman/src/APP/BelmanProject.java
+++ Belman/src/APP/BelmanProject.java
@@ -15,23 +15,23 @@ public class BelmanProject
      * Belman Project main metode
      * @param args the command line arguments
      */
-//    public static void main(String[] args)
-//    {
-//        EventQueue.invokeLater(new Runnable()
-//        {
-//            @Override
-//            public void run()
-//            {
-//                try
-//                {
-//                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//                    Overview.getInstance().setVisible(true);
-//                }
-//                catch (Exception e)
-//                {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//    }
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                    Overview.getInstance().setVisible(true);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
